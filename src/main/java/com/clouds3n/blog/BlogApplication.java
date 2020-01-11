d848ac34d21c848471676311a418d7f52c8eab1b
clouds3n <www.402645063@gmail.com>
2020-01-12 00:38:54 +0800

    chore(all): project configuration

--- src/main/java/com/clouds3n/blog/BlogApplication.java
+++ src/main/java/com/clouds3n/blog/BlogApplication.java
@@ -3,6 +3,9 @@ package com.clouds3n.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author CloudS3n
+ */
 @SpringBootApplication
 public class BlogApplication {
 
