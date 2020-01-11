b92323759c37446c2b215d68fb910c0c0f1508e2
clouds3n <www.402645063@gmail.com>
2020-01-12 00:50:33 +0800

    doc(sql): add dml for encouragement table

---
+++ src/main/resources/sql/DML.sql
@@ -0,0 +1,24 @@
+# encouragement
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('f5b30800c4cd476eb7ff08bdcbc2dc0b', 'Go Pro, Or Go Home.');
+
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('e604c74ed8fd414b8a38fb618088551a', '别人都在以你不知道的方式进步，你的进步只是别人的常态。');
+
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('3ee6269da2a34fba9194787940c9b3c0', '蜉，水虫也，朝生暮死。');
+
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('756f96450cf54ce7839530a4986df93e', '夫学须静也，才须学也，非学无以广才，非志无以成学。');
+
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('2f7e181b5e21417394b5463b1774b126', '不积跬步，无以至千里；不积小流，无以成江海。');
+
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('527aefba0b32495e99515f5069547a38', '一日无二晨，时过不再临。');
+
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('f9cd2a7b26304a20a9ee265fa1d5e975', 'Prices will rise, politicians will philander, you, too, will get old.');
+
+INSERT INTO `blog`.`encouragement` (`uuid`, `content`)
+VALUES ('f8741ae954fc4e48acc4a151347b2e51', '青，取之于蓝，而青于蓝；冰，水为之，而寒于水。');
