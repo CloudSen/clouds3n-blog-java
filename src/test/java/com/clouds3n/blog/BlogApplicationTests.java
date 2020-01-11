e50649ed0efd3b98a6583828a0fd941e1e07d72a
clouds3n <www.402645063@gmail.com>
2020-01-12 00:20:41 +0800

    tron(42): initial project

---
+++ src/test/java/com/clouds3n/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouds3n.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
