e0ae594fff3e3dcede7717b29a237e0c6e6f9a32
clouds3n <www.402645063@gmail.com>
2020-01-12 04:52:37 +0800

    feat(config): 配置p6sy,druid监控和mybatis-plus

--- src/main/java/com/clouds3n/blog/config/mybatis/MybatisPlusConfig.java
+++ src/main/java/com/clouds3n/blog/config/mybatis/MybatisPlusConfig.java
@@ -16,13 +16,13 @@ import javax.sql.DataSource;
  * @date 2020-01-12 1:52
  */
 @Configuration
-@MapperScan("com.clouds3n.blog.business.repository")
+@MapperScan("com.clouds3n.blog.business.*.mapper")
 public class MybatisPlusConfig {
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         Resource[] resources = new PathMatchingResourcePatternResolver()
-            .getResources("classpath*:mapper/*.xml");
+            .getResources("classpath*:mapper/**/*.xml");
         MybatisSqlSessionFactoryBean factoryBean = new MybatisSqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource);
         factoryBean.setMapperLocations(resources);
