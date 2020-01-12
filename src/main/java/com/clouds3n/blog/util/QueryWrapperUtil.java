b3425cd57059856d95e2421f6c67319ea1242106
clouds3n <www.402645063@gmail.com>
2020-01-13 00:43:49 +0800

    feat(util): 添加mybatis的query wrapper工具类

---
+++ src/main/java/com/clouds3n/blog/util/QueryWrapperUtil.java
@@ -0,0 +1,57 @@
+package com.clouds3n.blog.util;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.clouds3n.blog.common.QueryConditionDto;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * @author CloudS3n
+ * @date 2020-01-12 19:51
+ */
+public class QueryWrapperUtil {
+
+    public static <T> QueryWrapper<T> parseWhereSql(List<QueryConditionDto> conditionList, T type) throws IllegalAccessException {
+        QueryWrapper<T> queryWrapper = new QueryWrapper<>();
+        if (CollectionUtils.isNotEmpty(conditionList)) {
+            for (QueryConditionDto queryCondition : conditionList) {
+                switch (queryCondition.getQueryType()) {
+                    case EQ:
+                        queryWrapper.eq(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case NE:
+                        queryWrapper.ne(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case LIKE:
+                        queryWrapper.like(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case LEFT_LIKE:
+                        queryWrapper.likeLeft(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case RIGHT_LIKE:
+                        queryWrapper.likeRight(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case NOT_LIKE:
+                        queryWrapper.notLike(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case GT:
+                        queryWrapper.gt(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case LT:
+                        queryWrapper.lt(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case GE:
+                        queryWrapper.ge(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    case LE:
+                        queryWrapper.le(queryCondition.getColumn(), queryCondition.getValue());
+                        break;
+                    default:
+                        throw new IllegalAccessException("无法匹配查询类型");
+                }
+            }
+        }
+        return queryWrapper;
+    }
+}
