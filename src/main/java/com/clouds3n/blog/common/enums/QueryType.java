46e4a655cb562f87840a56c0fe39cc86d3995fa3
clouds3n <www.402645063@gmail.com>
2020-01-13 00:44:46 +0800

    feat(common/enum): 添加查询类型枚举，用于query wrapper util

---
+++ src/main/java/com/clouds3n/blog/common/enums/QueryType.java
@@ -0,0 +1,70 @@
+package com.clouds3n.blog.common.enums;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
+/**
+ * @author CloudS3n
+ * @date 2020-01-12 20:03
+ */
+public enum QueryType {
+    /**
+     * 全等
+     */
+    EQ("eq"),
+    /**
+     * 不等
+     */
+    NE("ne"),
+    /**
+     * 全模糊匹配查询
+     */
+    LIKE("like"),
+    /**
+     * 左模糊匹配查询
+     */
+    LEFT_LIKE("leftLike"),
+    /**
+     * 右模糊匹配查询
+     */
+    RIGHT_LIKE("rightLike"),
+    /**
+     * 全模糊不匹配查询
+     */
+    NOT_LIKE("notLike"),
+    /**
+     * 大于
+     */
+    GT("gt"),
+    /**
+     * 小于
+     */
+    LT("lt"),
+    /**
+     * 大于等于
+     */
+    GE("ge"),
+    /**
+     * 小于等于
+     */
+    LE("le"),
+    ;
+
+    @JsonValue
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    QueryType(String name) {
+        this.name = name;
+    }
+
+    public static QueryType parseQueryType(String name) {
+        return Arrays.stream(values())
+            .filter(queryType -> queryType.getName().equalsIgnoreCase(name))
+            .findAny().orElseThrow(() -> new IllegalArgumentException("无法解析搜索类型" + name));
+    }
+}
